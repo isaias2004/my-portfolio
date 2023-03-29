f39662f09f9820c1990a8ca4e7048567bce6c2a5
Isaias <lawicki76@gmail.com>
2023-03-29 20:21:53 -0300

    Add the sobremi component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header';
-import Container from '@mui/material/Container'
+import Container from '@mui/material/Container';
+import Sobremi from './components/Sobremi';
 
 function App() {
   return (
     <Container>
       <div className="App">
         <header className="App-header">
-          <Header></Header>
+          <Header/> 
+          <Sobremi/>
         </header>
       </div>
     </Container>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
 
-
-class Header extends Component {
-    render(){
-        return (
-        <div>
-            <h2>Isaias Lawicki</h2>
-                <ul className="lista-container">
-                    <li>Inicio</li>
-                    <li>Sobre Mi</li>
-                    <li>Habilidades</li>
-                    <li>Proyectos</li>
-                    <li>Contacto</li>
-                </ul>
+const Header = () => {
+  return (
+    <div className="menu-container">
+            <h2>Isaias Lawicki(logo)</h2>
+            <ul className="lista-container">
+                <li>Inicio</li>
+                <li>Sobre Mi</li>
+                <li>Habilidades</li>
+                <li>Proyectos</li>
+                <li>Contacto</li>
+            </ul>
         </div>
-        )
-    }
+  )
 }
 
-export default Header;
+export default Header
