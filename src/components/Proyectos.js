d028e49034f30a08665f2e59b0aed750c0269401
Isaias <lawicki76@gmail.com>
2023-05-19 20:21:46 -0300

    Add a proyectos component

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import logocuidar from '../img/cuidar-corazon.jpeg'
 
 const Proyectos = () => {
   return (
-    <div>Proyectos</div>
+    <div className='proyectos-container'>
+      <h2>Proyectos personales</h2>
+      <p>Algunos proyectos desarrollados con distintas tecnologías.</p>
+
+      <div className='container-general'>
+          <div className='container-individual'>
+            <a target="_blank" href='https://cuidarconscientemujer.com/'>
+              <img id='logocuidar' src={logocuidar} /></a>
+              <h3>Cuidar Consciente</h3>
+              <p>cuidarconscientemujer.com/</p>
+          </div>
+          <div className='container-individual'>
+            <a target="_blank" href='https://cuidarconscientemujer.com/'>
+              <img id='logocuidar' src={logocuidar} /></a>
+              <h3>Cuidar Consciente</h3>
+              <p>cuidarconscientemujer.com/</p>
+          </div>
+          <div className='container-individual'>
+            <a target="_blank" href='https://cuidarconscientemujer.com/'>
+              <img id='logocuidar' src={logocuidar} /></a>
+              <h3>Cuidar Consciente</h3>
+              <p>cuidarconscientemujer.com/</p>
+          </div>
+          <div className='container-individual'>
+            <a target="_blank" href='https://cuidarconscientemujer.com/'>
+              <img id='logocuidar' src={logocuidar} /></a>
+              <h3>Cuidar Consciente</h3>
+              <p>cuidarconscientemujer.com/</p>
+          </div>
+      </div>
+    </div>  
   )
 }
 
